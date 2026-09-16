68c91a09bad759bcc20bdd26cd09aa8b90ca5495
Omara Morgan <omara.morgan26@gmail.com>
2026-09-16 08:55:15 -0500

    Make admin a separate page entry

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
   server: { host: true, port: 5173 },
   optimizeDeps: {
     exclude: ['three'],
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'admin/index.html'),
+      },
+    },
   },
 });
